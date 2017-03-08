50585d2824116b9ee08552e68a07fb6264d3a228
camboui <mathias.branger@gmail.com>
2017-03-07 21:59:59 -0500

    optime code

--- client/components/colliderComponent.js
+++ client/components/colliderComponent.js
@@ -19,10 +19,10 @@ define([
   // ## Variable *colliders*
   // On conserve ici une référence vers toutes les instances
   // de cette classe, afin de déterminer si il y a collision.
-  //Divise la carte en 50*50 cases
 
-  const xDivider = 20;
-  const yDivider = 20;
+  //Divise la carte en xDivider par yDivider cases de tailles égales
+  const xDivider = 30;
+  const yDivider = 30;
   const grid = new Array (xDivider);
   const window = document.getElementById('canvas');
   const cellDim = {x : window.width/xDivider, y :window.height/yDivider};
@@ -37,9 +37,6 @@ define([
 
       //Initialiser la grille avec des cellules vides
       if(grid[0] == undefined){
-        const Scene = require('scene');
-        const target = Scene.current.findObject('Camera');
-        console.log(target);
         for(var i = 0 ; i < grid.length ; i++){
           grid[i] = new Array(yDivider);
           for(var j = 0 ; j < grid[0].length ; j++){
@@ -47,7 +44,8 @@ define([
           }
         }
       }
-
+      this.previousTP = {x : -1, y:-1};
+      this.previousBR = {x : -1, y:-1};
       this.occupyingCells = [];
       this.flag = descr.flag;
       this.mask = descr.mask;
@@ -83,110 +81,116 @@ define([
       var topLeftCell = {x : Math.trunc(this.area.xMin/cellDim.x), y:Math.trunc(this.area.yMin/cellDim.y)};
       var bottomRightCell = {x : Math.trunc(this.area.xMax/cellDim.x), y:Math.trunc(this.area.yMax/cellDim.y)};
 
-      //On enleve les anciennes traces de l'objet dans les cellules
-      if(this.occupyingCells.length!=0)
-      {
-        this.occupyingCells.forEach((cell) => {
-          var deleteIndex = grid[cell.x][cell.y].indexOf(this);
-          grid[cell.x][cell.y].splice(deleteIndex, 1);
-        });
-        this.occupyingCells =[];
-      }
-
-      //Si on est bien dans le tableau
-      if(this.inBounds(topLeftCell) && this.inBounds(bottomRightCell))
-      {
-        //Le collider peut être présent dans plusieurs cellules à la fois
-        for(var i =  topLeftCell.x ; i <= bottomRightCell.x ; i++){
-          for(var j = topLeftCell.y ; j <= bottomRightCell.y ; j++){
-            grid[i][j].push(this);
-            this.occupyingCells.push({x : i, y:j});
+      //OPTIMISATION : On ne met à jour que s'il y a eu déplacement dans le tableau
+      if(this.previousTP.x != topLeftCell.x || this.previousTP.y != topLeftCell.y
+        || this.previousBR.x != bottomRightCell.x || this.previousBR.y != bottomRightCell.y){
+          //On enleve les anciennes traces de l'objet dans les cellules
+          if(this.occupyingCells.length!=0)
+          {
+            this.occupyingCells.forEach((cell) => {
+              var deleteIndex = grid[cell.x][cell.y].indexOf(this);
+              grid[cell.x][cell.y].splice(deleteIndex, 1);
+            });
+            this.occupyingCells =[];
           }
+
+          //Si on est bien dans le tableau
+          if(this.inBounds(topLeftCell) && this.inBounds(bottomRightCell))
+          {
+            //Le collider peut être présent dans plusieurs cellules à la fois
+            for(var i =  topLeftCell.x ; i <= bottomRightCell.x ; i++){
+              for(var j = topLeftCell.y ; j <= bottomRightCell.y ; j++){
+                grid[i][j].push(this);
+                this.occupyingCells.push({x : i, y:j});
+              }
+            }
+            //console.log(this.occupyingCells)
+          }
+          this.previousTP = topLeftCell;
+          this.previousBR = bottomRightCell;
         }
-        //console.log(this.occupyingCells)
-      }
 
-      if (!this.handler) {
-        return;
-      }
-
-      //Checker les collisions avec tous les objets qui ont des cellules communes
-      const area = this.area;
-      var potentialColliders = this.getCollidersInCells();
-
-      potentialColliders.forEach((c) => {
-        if (this.isUselessObject(c)) {
+        if (!this.handler) {
           return;
         }
-        // vérifie les polygones anglobants
-        if (area.intersectsWith(c.area)) {
-          //this.logCollision("COLLISION",c);
-          this.handler.onCollision(c);
-        }
-      });
 
-    }
+        //Checker les collisions avec tous les objets qui ont des cellules communes
+        const area = this.area;
+        var potentialColliders = this.getCollidersInCells();
 
-    getCollidersInCells()
-    {
-      var tab = [];
-      this.occupyingCells.forEach((cell) => {
-        tab = tab.concat(grid[cell.x][cell.y]);
-      });
-      return tab;
-    }
-
-    inBounds(arrayPos){
-      return (arrayPos.x >=0 && arrayPos.x <yDivider && arrayPos.y >=0 && arrayPos.y <xDivider);
-    }
-
-    isUselessObject(otherCollider)
-    {
-      /*On ne teste que les objets intéressants, à savoir
-      les objets actifs
-      dont le composant est actif
-      et dont le masque autorise la collision avec le flag de l'objet*/
-      var res =  (otherCollider === this ||
-        !otherCollider.enabled ||
-        !otherCollider.owner.active ||
-        !(this.mask & otherCollider.flag));
-
-        //  if(!res) {this.logCollision("Potential interaction",otherCollider);}
-        return res;
-      }
-
-      logCollision(message, otherCollider){
-        const rupee = otherCollider.owner.getComponent('Rupee');
-        const heart = otherCollider.owner.getComponent('Heart');
-        const chicken = otherCollider.owner.getComponent('Chicken');
-
-        var text =  message+" between " + this.owner.getComponent('Player').name + " and ";
-        if (rupee) {
-          text+= "rupee";
-        }
-        if (heart) {
-          text+= "heart";
-        }
-        if (chicken) {
-          text+= "chicken";
-        }
-        console.log(text);
-      }
-
-      // ## Propriété *area*
-      // Cette fonction calcule l'aire courante de la zone de
-      // collision, après avoir tenu compte des transformations
-      // effectuées sur les objets parent.
-      get area() {
-        const position = this.owner.getComponent('Position').worldPosition;
-        return new Rectangle({
-          x: position[0],
-          y: position[1],
-          width: this.size.w,
-          height: this.size.h,
+        potentialColliders.forEach((c) => {
+          if (this.isUselessObject(c)) {
+            return;
+          }
+          // vérifie les polygones anglobants
+          if (area.intersectsWith(c.area)) {
+            //this.logCollision("COLLISION",c);
+            this.handler.onCollision(c);
+          }
         });
-      }
-    }
 
-    return ColliderComponent;
-  });
+      }
+
+      getCollidersInCells()
+      {
+        var tab = [];
+        this.occupyingCells.forEach((cell) => {
+          tab = tab.concat(grid[cell.x][cell.y]);
+        });
+        return tab;
+      }
+
+      inBounds(arrayPos){
+        return (arrayPos.x >=0 && arrayPos.x <yDivider && arrayPos.y >=0 && arrayPos.y <xDivider);
+      }
+
+      isUselessObject(otherCollider)
+      {
+        /*On ne teste que les objets intéressants, à savoir
+        les objets actifs
+        dont le composant est actif
+        et dont le masque autorise la collision avec le flag de l'objet*/
+        var res =  (otherCollider === this ||
+          !otherCollider.enabled ||
+          !otherCollider.owner.active ||
+          !(this.mask & otherCollider.flag));
+
+          //  if(!res) {this.logCollision("Potential interaction",otherCollider);}
+          return res;
+        }
+
+        logCollision(message, otherCollider){
+          const rupee = otherCollider.owner.getComponent('Rupee');
+          const heart = otherCollider.owner.getComponent('Heart');
+          const chicken = otherCollider.owner.getComponent('Chicken');
+
+          var text =  message+" between " + this.owner.getComponent('Player').name + " and ";
+          if (rupee) {
+            text+= "rupee";
+          }
+          if (heart) {
+            text+= "heart";
+          }
+          if (chicken) {
+            text+= "chicken";
+          }
+          console.log(text);
+        }
+
+        // ## Propriété *area*
+        // Cette fonction calcule l'aire courante de la zone de
+        // collision, après avoir tenu compte des transformations
+        // effectuées sur les objets parent.
+        get area() {
+          const position = this.owner.getComponent('Position').worldPosition;
+          return new Rectangle({
+            x: position[0],
+            y: position[1],
+            width: this.size.w,
+            height: this.size.h,
+          });
+        }
+      }
+
+      return ColliderComponent;
+    });
